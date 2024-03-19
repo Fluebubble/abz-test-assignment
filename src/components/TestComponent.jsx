b1cf9c8086527c05f1874e583b0903fc5350d9b8
Fluebubble <Trots.anatoly@gmail.com>
2024-03-20 01:03:57 +0200

    create-reat-app changed to vite

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,9 @@
+import styles from './TestComponent.module.css';
+
+export const TestComponent = () => {
+  return(
+    <div className={styles.testComp}>
+      <p>RandomText</p>
+    </div>
+  )
+}
