ba0eab6313c716a8be4ac6ff911b2ac0804be53c
Fluebubble <Trots.anatoly@gmail.com>
2024-03-20 13:47:48 +0200

    test clear commit dev CRA branch

--- src/components/TestComponent.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './TestComponent.module.scss';
-
-export const TestComponent = () => {
-  return(
-    <div className={styles.testComp}>
-      <p>RandomText</p>
-    </div>
-  )
-}
