f44fdbb89c13f8122eb1e64256a198da9014caf4
Fluebubble <Trots.anatoly@gmail.com>
2024-04-03 01:27:21 +0300

    EmployeesSection renamed to UsersSection + meta tag added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
-import { EmployeesSection } from './components/EmployeesSection/EmployeesSection';
+import { EmployeesSection } from './components/UsersSection/UsersSection';
 import { Container } from './components/Container/Container';
 import { FormSection } from './components/FormSection/FormSection';
 import { UsersProvider } from './context/context';
